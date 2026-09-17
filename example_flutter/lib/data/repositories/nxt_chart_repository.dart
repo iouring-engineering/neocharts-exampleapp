0b5f16a632972f28149b6cf3a5ca090242df31f9
Pranav Murali <pranavmurali@iouring.com>
2026-09-17 16:15:57 +0530

    feat: rename example marketTiming host getter to marketTimings

--- example_flutter/lib/data/repositories/nxt_chart_repository.dart
+++ example_flutter/lib/data/repositories/nxt_chart_repository.dart
@@ -174,12 +174,14 @@ class NxtChartRepository implements ChartInterface {
   }
 
   @override
-  String get marketTiming {
+  String get marketTimings {
     return jsonEncode({
-      'timezone': 'Asia/Kolkata',
-      'sessions': List.generate(7, (_) => ['0000-2359']),
-      'holidays': <String>[],
-      'special': <String, dynamic>{},
+      'NSE': {
+        'timezone': 'Asia/Kolkata',
+        'sessions': List.generate(7, (_) => ['0000-2359']),
+        'holidays': <String>[],
+        'special': <String, dynamic>{},
+      },
     });
   }
 
